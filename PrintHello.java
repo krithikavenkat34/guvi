b2e679b8fcaa8911158a9efec8327d3440e01c60
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2018-08-27 16:51:31 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,13 @@
+public class PrintHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the num:");
+       int n=sc.nextInt();
+       for(int i=0;i<n;i++) {
+    	   System.out.println("Hello World");
+       }
+	}
+
+}
