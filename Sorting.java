b0ff98a0d5d1505409bc9e1ff71165a5140ac6ae
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-02-19 18:19:00 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Sorting {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int a[]=new int[n];
+       int tem=0;
+       for(int i=0;i<n;i++) 
+         a[i]=sc.nextInt();
+       for(int j=0;j<n-1;j++)
+       {
+           if(a[j]>a[j+1]) 
+           {
+               tem=a[j];
+               a[j]=a[j+1];
+               a[j+1]=tem;
+           }
+       }
+        for(int i=0;i<n;i++) 
+         System.out.print(a[i]+" ");
+    }
+}
