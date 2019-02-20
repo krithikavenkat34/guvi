8c11ff05ecd7ed33340bc566a934ca3535b5f2c8
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-02-20 18:43:48 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Max {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[10];
+        for(int i=0;i<10;i++)
+         a[i]=sc.nextInt();
+         int max=a[0];
+        for(int i=0;i<10;i++)
+        {
+            if(a[i]>max)
+              max=a[i];
+        }
+        System.out.println(max);
+    }
+}
