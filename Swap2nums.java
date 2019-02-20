48327064ab79099d4b814ae48cb602d4da22f942
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-02-20 18:04:57 +0530

    Create Swap2nums.java

---
+++ Swap2nums.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Swap2nums {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.print(a+" "+b);
+    }
+}
+
