86371eaf3b30f8d366fcea8343b081aaaef56ddb
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-02-21 19:31:17 +0530

    Create PrintString.java

---
+++ PrintString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PrintString
+{
+  public static void main(String []args)
+  {
+     Scanner sc=new Scanner(System.in);
+     String s=sc.next();
+     int n=sc.nextInt();
+     for(int i=0;i<n;i++)
+      System.out.println(s);
+  }
+}
