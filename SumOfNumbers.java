9e5f79b2efef6c91dfa7bda7330e74df4e3cce0f
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-03-03 12:07:34 +0530

    Create SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SumOfNumbers
+{
+    public static void main(String []args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0,rem=0;
+        while(n!=0)
+        {
+            rem=n%10;
+            sum+=rem;
+            n/=10;
+        }
+        System.out.println(sum);
+      }
+}
