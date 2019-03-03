7484ace9e6d89a81514e3d5a0d99a693627cbd1e
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-03-03 10:21:21 +0530

    Create FindRange110.java

---
+++ FindRange110.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FindRange110 {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>=1&&x<=10) 
+        System.out.println("yes");
+        else
+        System.out.println("no");
+    }
+}
