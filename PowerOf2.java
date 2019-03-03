82bb37ee07550dca27615f9155fcdad9abed1d62
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-03-03 10:41:39 +0530

    Create PowerOf2.java

---
+++ PowerOf2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PowerOf2 {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x%2==0)
+        System.out.println("yes");
+        else System.out.println("no");
+    }
+}
