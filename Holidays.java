cb0417b2747864d24d57791092033c10a7ffa825
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-03-11 18:14:25 +0530

    Create Holidays.java

---
+++ Holidays.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Holidays
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+   if(s1.equals("Sunday")||s1.equals("Saturday"))
+     System.out.println("Yes");
+     else System.out.println("No");
+    }
+}
