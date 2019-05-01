c1d78b50a0c40af838870dfcedbc65760689d7b7
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-01 17:29:26 +0530

    Create RangeEndValue.java

---
+++ RangeEndValue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class RangeEndValue {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+         int y=x/10;
+         y+=1;
+         System.out.println(y*10);
+        }
+}
