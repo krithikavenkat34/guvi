d9115d8eb7a03247472dd2e8ecf2016f7d6a157e
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-01 15:49:40 +0530

    Create NoOftimes.java

---
+++ NoOftimes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+       int s=sc.nextInt();
+        int arr[]=new int[x];
+        for(int i=0;i<x;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<x;i++)
+        {
+            if(arr[i]==s)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
