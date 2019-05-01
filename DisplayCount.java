c71ff86c59003474c08f41bb9e02616ef278c07f
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-01 16:25:25 +0530

    Create DisplayCount.java

---
+++ DisplayCount.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class DisplayCount {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int x=sc.nextInt();
+        System.out.println(s.substring(0,x));
+    }
+}
