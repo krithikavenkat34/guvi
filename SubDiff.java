b01cec9dd55715fa6753a8563f3969ac8be63289
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-01 18:13:44 +0530

    Create SubDiff.java

---
+++ SubDiff.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SubDiff {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x-y;
+        if(z%2==0)
+         System.out.println("yes");
+         else System.out.println("no");
+    }
+}
