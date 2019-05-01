b28f5760b07cbeff0ebc24064078c219ae8547d3
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-01 17:45:07 +0530

    Create DiviBy7.java

---
+++ DiviBy7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class DiviBy7 {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x%7==0)
+         System.out.println("yes");
+         else System.out.println("no");
+    }
+}
