8080ecfb1b7f54a0ad126a7e109bdb1adb1e1a0c
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-02 15:04:45 +0530

    Create FactorsInRange.java

---
+++ FactorsInRange.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FactorsInRange {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=1;i<=x;i++)
+        {
+            if(x%i==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
