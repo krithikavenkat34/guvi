d9cf3c518ce369ce2f8f3b36617c0e77bce7e5e5
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-02 11:25:48 +0530

    Create ConvertFtoI.java

---
+++ ConvertFtoI.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ConvertFtoI {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        float n=sc.nextFloat();
+        int c=(int)n;
+        System.out.println(c+1);
+       
+    }
+}
