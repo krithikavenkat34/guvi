8cc57e2ee0ecbe5ef0d922dfd5228aa99ab1e37e
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-08 15:02:20 +0530

    Create Decimal5.java

---
+++ Decimal5.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        float x=sc.nextFloat();
+        float y=sc.nextFloat();
+        System.out.printf("%.5f",x*y);
+    }
+}
