a0e5f3e25304f93053b3b376203a253ac3ac0f7d
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-05-09 18:39:31 +0530

    Create ModDiv.java

---
+++ ModDiv.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        char ch=sc.next().charAt(0);
+        int b=sc.nextInt();
+        if(ch=='%')
+        {
+            System.out.println(a%b);
+        }
+        else if(ch=='/')
+        {
+            System.out.println(a/b);
+        }
+    }
+}
