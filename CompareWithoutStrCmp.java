a5113f04b6b622c051c4973075f7446a567cbe3c
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-06-26 10:45:10 +0530

    Create CompareWithoutStrCmp.java

---
+++ CompareWithoutStrCmp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CompareWithoutStrCmp
+{
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.next();
+        String str2=sc.next();
+        if(str1.equals(str2))
+        System.out.println("yes");
+        else System.out.println("no");
+    }
+}
