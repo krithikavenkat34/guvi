4d7bca9bc14e1a689702a4920a43f9843cc8f0e2
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-06-26 10:07:14 +0530

    Create DivByInnerValues.java

---
+++ DivByInnerValues.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class DivByInnerValues {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0) count++;
+        }
+        if(count>=1)
+        System.out.println("yes");
+        else
+        System.out.println("no");
+    }
+}
