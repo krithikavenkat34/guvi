496d9402a7583cc2a09b7f87dc9f999ccbf04b44
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-07-03 11:08:54 +0530

    Update SmallestThanK.java

--- SmallestThanK.java
+++ SmallestThanK.java
@@ -1,19 +1,31 @@
 import java.util.*;
-public class SmallestThanK {
+public class MyClass {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         int a[]=new int[x];
+        int arr[]=new int[x];
         int k=sc.nextInt();
         for(int i=0;i<x;i++)
         {
             a[i]=sc.nextInt();
         }
+        int l=0;
         for(int i=0;i<x;i++)
         {
             if(a[i]<k)
             {
-                  System.out.print(a[i]+" ");
+                 arr[l]=a[i];
+                 l++;
+                 
+            }
+        }
+        Arrays.sort(arr);
+         for(int i=0;i<x;i++)
+        {
+            if(arr[i]!=0)
+            {
+            System.out.println(arr[i]);
             }
         }
     }
