3388787aa2e8e5637a148eab373b1b2ce86c9b02
krithikavenkat34 <42089620+krithikavenkat34@users.noreply.github.com>
2019-08-08 12:24:45 +0530

    Create BinaryMax.java

---
+++ BinaryMax.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class BinaryMax {
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int x=sc.nextInt();
+      int a[]=new int[x];
+      for(int i=0;i<x;i++)
+      {
+          a[i]=sc.nextInt();
+      }
+      int p=0;
+      Arrays.sort(a);
+    int max=Integer.parseInt(Integer.toBinaryString(a[0]));
+     for(int i=0;i<x;i++)
+     {
+       if(max<Integer.parseInt(Integer.toBinaryString(a[i])))
+       {
+           max=Integer.parseInt(Integer.toBinaryString(a[i]));
+       }
+     }
+     String max1=String.valueOf(max);
+     System.out.println(Integer.parseInt(max1,2));
+    }
+}
